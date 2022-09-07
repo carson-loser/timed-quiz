58f6ade49602180c60d0614227d77032758f4d79
carson-loser <carson.loser@gmail.com>
2022-09-07 16:34:17 -0500

    started over on javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,76 +1,168 @@
-var start_btn = document.querySelector(".start-btn");
-var hiscore_btn = document.querySelector(".hiScoreBtn");
+var question1 = {
+  question: "Q1",
+  answers: ["ans1", "ans2", "ans3", "ans4"],
+  correctAnswer: "ans3"
+};
 
-// add an event listener for clicking start quiz
+var question2 = {
+  question: "Q2",
+  answers: ["ans1", "ans2", "ans3", "ans4"],
+  correctAnswer: "ans1"
+};
 
-start_btn.addEventListener("click", function (event) {
-  event.preventDefault()
-  document.querySelector(".question1").style = "display:block";
-  document.querySelector(".start-section").style = "display:none";
-  setTime();
-  que2();
-  // correctQue();
-});
+var question3 = {
+  question: "Q3",
+  answers: ["ans1", "ans2", "ans3", "ans4"],
+  correctAnswer: "ans3"
+};
 
-function que2() {
-  var answerButtons = document.querySelectorAll(".ans1-button");
-  console.log(answerButtons);
+var question4 = {
+  question: "Q4",
+  answers: ["ans1", "ans2", "ans3", "ans4"],
+  correctAnswer: "ans4"
+};
+
+var question5 = {
+  question: "Q5",
+  answers: ["ans1", "ans2", "ans3", "ans4"],
+  correctAnswer: "ans2"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var start_btn = document.querySelector(".start-btn");
+// var hiscore_btn = document.querySelector(".hiScoreBtn");
+// var correctBtns = document.querySelectorAll(".correct-btn");
+// var correctBtns = ["script", "myFunction()", "both", "both head and body work", "terminal"];
+
+// // add an event listener for clicking start quiz
+
+// start_btn.addEventListener("click", function (event) {
+//   event.preventDefault()
+//   document.querySelector(".question1").style = "display:block";
+//   document.querySelector(".start-section").style = "display:none";
+//   setTime();
+//   que2();
+
+// });
+
+// function que2() {
+//   var answerButtons = document.querySelectorAll(".ans1-button");
+//   console.log(answerButtons);
+  
+//   // correctBtns[0].addEventListener("click", function (event) {
+//   //   event.preventDefault()
+  
+
+
+
+
 
   // function correctQue()
   // var correctButtons = document.querySelectorAll(".correct-btn");
   // console.log(correctButtons);
-  for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function (event) {
-      event.preventDefault()
-      document.querySelector(".question2").style = "display:block";
-      document.querySelector(".question1").style = "display:none";
-      // document.querySelector(".correct-btn").style ="background-color:green";
-    });
+  
+  
+  
+//   for (let i = 0; i < answerButtons.length; i++) {
+//     answerButtons[i].addEventListener("click", function (event) {
+//       event.preventDefault()
+//       document.querySelector(".question2").style = "display:block";
+//       document.querySelector(".question1").style = "display:none";
+//       // document.querySelector(".correct-btn").style ="background-color:green";
+//     });
 
-  }
-  que3();
-}
+//   }
+//   que3();
+// }
 
-function que3() {
-  var answerButtons = document.querySelectorAll(".ans2-button");
-  console.log(answerButtons);
+// function que3() {
+//   var answerButtons = document.querySelectorAll(".ans2-button");
+//   console.log(answerButtons);
 
-  for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function (event) {
-      event.preventDefault()
-      document.querySelector(".question3").style = "display:block";
-      document.querySelector(".question2").style = "display:none";
-    });
-  }
-  que4();
-}
+//   for (let i = 0; i < answerButtons.length; i++) {
+//     answerButtons[i].addEventListener("click", function (event) {
+//       event.preventDefault()
+//       document.querySelector(".question3").style = "display:block";
+//       document.querySelector(".question2").style = "display:none";
+//     });
+//   }
+//   que4();
+// }
 
-function que4() {
-  var answerButtons = document.querySelectorAll(".ans3-button");
-  console.log(answerButtons);
+// function que4() {
+//   var answerButtons = document.querySelectorAll(".ans3-button");
+//   console.log(answerButtons);
 
-  for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function (event) {
-      event.preventDefault()
-      document.querySelector(".question4").style = "display:block";
-      document.querySelector(".question3").style = "display:none";
-    });
-  }
-  que5();
-}
+//   for (let i = 0; i < answerButtons.length; i++) {
+//     answerButtons[i].addEventListener("click", function (event) {
+//       event.preventDefault()
+//       document.querySelector(".question4").style = "display:block";
+//       document.querySelector(".question3").style = "display:none";
+//     });
+//   }
+//   que5();
+// }
 
-function que5() {
-  var answerButtons = document.querySelectorAll(".ans4-button");
-  console.log(answerButtons);
+// function que5() {
+//   var answerButtons = document.querySelectorAll(".ans4-button");
+//   console.log(answerButtons);
 
-  for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function (event) {
-      event.preventDefault()
-      document.querySelector(".question5").style = "display:block";
-      document.querySelector(".question4").style = "display:none";
-    });
-  }
-}
+//   for (let i = 0; i < answerButtons.length; i++) {
+//     answerButtons[i].addEventListener("click", function (event) {
+//       event.preventDefault()
+//       document.querySelector(".question5").style = "display:block";
+//       document.querySelector(".question4").style = "display:none";
+//     });
+//   }
+// }
 
 
 
